43a4aab21de44da257a2e8037b6a960ddd88d98e
ZhanLiangUF <shanliangko7@gmail.com>
2023-11-10 12:53:15 -0500

    Validparenthesses

---
+++ validparentheses.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+public class validparentheses {
+      public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+
+        for (char c: s.toCharArray()) {
+            if ( c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.peek();
+                if ((c == ')' && top == '(') || (c == ']' && top == '[') || (c == '}' && top == '{')) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
