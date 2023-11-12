02abded7d7b85f9808f3faa9f3275e6bde8416b8
ZhanLiangUF <shanliangko7@gmail.com>
2023-11-12 08:39:28 -0500

    isanagram

---
+++ isanagram.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class isanagram {
+      public boolean isAnagram(String s, String t) {
+       Map<Character, Integer> map = new HashMap<>();
+       for (char x: s.toCharArray()) {
+           map.put(x, map.getOrDefault(x, 0) + 1);
+       }
+       for (char x: t.toCharArray()) {
+           map.put(x, map.getOrDefault(x, 0) - 1);
+       } 
+
+       for (int val : map.values()) {
+           if (val != 0) {
+               return false;
+           }
+       }
+       return true;
+    }
+}
